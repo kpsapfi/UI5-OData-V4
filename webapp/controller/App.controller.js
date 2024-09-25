cc527f3580266d2b1aa068e4e3373308c739f23d
kalen <kalen.peredo@sapfioneer.com>
2024-09-25 10:18:31 -0600

    Add search and sort functionality
    Add onSearch() & onSort() functions to controller. Update view with button and search field, changing i8n to reflect the new variables

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,8 +2,12 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -13,7 +17,8 @@ sap.ui.define([
 		 */
 		onInit : function () {
 			var oJSONData = {
-					busy : false
+					busy : false,
+					order: 0
 				},
 				oModel = new JSONModel(oJSONData);
 
@@ -29,6 +34,33 @@ sap.ui.define([
 			}
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		onSearch : function () {
+			let oView = this.getView();
+			let sValue = oView.byId("searchField").getValue();
+			let oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+
+			oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+
+
+		},
+
+		onSort : function () {
+			let oView = this.getView();
+			let aStates = [undefined, "asc", "desc"];
+			let aStateTextIds = ["sortNone", "sortAscending", "sortDescending"];
+			let sMessage;
+			let iOrder = oView.getModel("appView").getProperty("/order");
+
+			iOrder = (iOrder + 1) % aStates.length;
+			let sOrder = aStates[iOrder];
+
+			oView.getModel("appView").setProperty("/order", iOrder);
+			oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter ("LastName", sOrder === "desc"));
+			
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			MessageToast.show(sMessage);
 		},
 
 		_getText : function (sTextId, aArgs) {
