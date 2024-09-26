4466199d2bcc12c2c4a3cc5465bc57233c22b1ca
kalen <kalen.peredo@sapfioneer.com>
2024-09-26 14:12:46 -0600

    Add ability to create and edit an entry
    Add new event handlers to make creation and editing possible. New entry send to the backend. Added method to submit information in a batch.  Toolbar appears in the footer when editing the entry.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -16,13 +16,54 @@ sap.ui.define([
 		 *  Hook for initializing the controller
 		 */
 		onInit : function () {
-			var oJSONData = {
+			let oMessageManager = sap.ui.getCore().getMessageManager();
+			let	oMessageModel = oMessageManager.getMessageModel();
+			let	oMessageModelBinding = oMessageModel.bindList("/", undefined, [],
+					new Filter("technical", FilterOperator.EQ, true));
+			let	oViewModel = new JSONModel({
 					busy : false,
-					order: 0
-				},
-				oModel = new JSONModel(oJSONData);
+					hasUIChanges : false,
+					usernameEmpty : true,
+					order : 0
+				});
+			this.getView().setModel(oViewModel, "appView");
+			this.getView().setModel(oMessageModel, "message");
 
-			this.getView().setModel(oModel, "appView");
+			oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
+			this._bTechnicalErrors = false;
+		},
+
+		onCreate : function () {
+			let oList = this.byId("peopleList");
+			let	oBinding = oList.getBinding("items");
+			let	oContext = oBinding.create({
+					"UserName" : "",
+					"FirstName" : "",
+					"LastName" : "",
+					"Age" : "18"
+			});
+
+			this._setUIChanges();
+			this.getView().getModel("appView").setProperty("/usernameEmpty", true);
+
+			oList.getItems().some(function (oItem) {
+				if (oItem.getBindingContext() === oContext) {
+					oItem.focus();
+					oItem.setSelected(true);
+					return true;
+				}
+			});
+		},
+
+		onInputChange : function (oEvt) {
+			if (oEvt.getParameter("escPressed")) {
+				this._setUIChanges();
+			} else {
+				this._setUIChanges(true);
+				if (oEvt.getSource().getParent().getBindingContext().getProperty("UserName")) {
+					this.getView().getModel("appView").setProperty("/usernameEmpty", false);
+				}
+			}
 		},
 
 		onRefresh : function () {
@@ -34,6 +75,30 @@ sap.ui.define([
 			}
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+
+		onResetChanges : function () {
+			this.byId("peopleList").getBinding("items").resetChanges();
+			this._bTechnicalErrors = false; 
+			this._setUIChanges();
+		},
+
+		onSave : function () {
+			let fnSuccess = function () {
+				this._setBusy(false);
+				MessageToast.show(this._getText("changesSentMessage"));
+				this._setUIChanges(false);
+			}.bind(this);
+
+			let fnError = function (oError) {
+				this._setBusy(false);
+				this._setUIChanges(false);
+				MessageBox.error(oError.message);
+			}.bind(this);
+
+			this._setBusy(true); // Lock UI until submitBatch is resolved.
+			this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
+			this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
 		},
 
 		onSearch : function () {
@@ -63,9 +128,52 @@ sap.ui.define([
 			MessageToast.show(sMessage);
 		},
 
+		onMessageBindingChange : function (oEvent) {
+			var aContexts = oEvent.getSource().getContexts(),
+				aMessages,
+				bMessageOpen = false;
+
+			if (bMessageOpen || !aContexts.length) {
+				return;
+			}
+
+			// Extract and remove the technical messages
+			aMessages = aContexts.map(function (oContext) {
+				return oContext.getObject();
+			});
+			sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+			this._setUIChanges(true);
+			this._bTechnicalErrors = true;
+			MessageBox.error(aMessages[0].message, {
+				id : "serviceErrorMessageBox",
+				onClose : function () {
+					bMessageOpen = false;
+				}
+			});
+
+			bMessageOpen = true;
+		},
+
 		_getText : function (sTextId, aArgs) {
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 
+		},
+
+		_setUIChanges : function (bHasUIChanges) {
+			if (this._bTechnicalErrors) {
+				// If there is currently a technical error, then force 'true'.
+				bHasUIChanges = true;
+			} else if (bHasUIChanges === undefined) {
+				bHasUIChanges = this.getView().getModel().hasPendingChanges();
+			}
+			var oModel = this.getView().getModel("appView");
+			oModel.setProperty("/hasUIChanges", bHasUIChanges);
+		},
+
+		_setBusy : function (bIsBusy) {
+			let oModel = this.getView().getModel("appView");
+			oModel.setProperty("/busy", bIsBusy);
 		}
 
 	});
